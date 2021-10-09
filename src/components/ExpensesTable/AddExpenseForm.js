1853a105c4e7d39edccda33cd3ce99985dc257b4
AndyLe <andyhongles@gmail.com>
2021-10-09 00:13:16 -0700

    description cost input expenses table

--- src/components/ExpensesTable/AddExpenseForm.js
+++ src/components/ExpensesTable/AddExpenseForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const AddExpenseForm = () => {
     const [fullName, setFullName] = useState("");
@@ -7,17 +7,41 @@ const AddExpenseForm = () => {
     const [cost, setCost] = useState(0);
 
     const selectCategoryHandler = (event) => {
-        setCategory(event.target.value)
+        setCategory(event.target.value);
     };
+
+    const changeDescriptionHandler = (event) => {
+        setDescription(event.target.value);
+    }
+
+    const costChangeHandler = (event) => {
+        setCost(event.target.value);
+    }
     return (
         <form>
             <select value={category} onChange={selectCategoryHandler}>
-                <option value="" disabled selected hidden>Choose a category</option>
+                <option value="" disabled selected hidden>
+                    Choose a category
+                </option>
                 <option value="food">Food</option>
                 <option value="travel">Travel</option>
                 <option value="health">Health</option>
                 <option value="supplies">Supplies</option>
             </select>
+            <input
+                type="text"
+                placeholder="Description"
+                required="required"
+                value={description}
+                onChange={changeDescriptionHandler}
+            />
+            <input
+                type="number"
+                placeholder="Cost"
+                required="required"
+                value={cost}
+                onChange={costChangeHandler}
+            />
         </form>
     );
 };
